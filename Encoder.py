000161f4b3302e0aa10a7bf8528b19438c8e6f93
Martin Fischer <martin.fis@gmx.net>
2016-06-10 09:42:42 +0200

    implements Encoder

--- Encoder.py
+++ Encoder.py
@@ -2,18 +2,23 @@ import sys
 from Crypto.Cipher import AES
 from utils import extend_string_to_base
 
-password = extend_string_to_base(sys.argv[1], 16)
-iv = extend_string_to_base(sys.argv[2], 16)
+class Encoder:
+    """"Simple Proxy for Crypto.Cipher"""
 
-source_file = open(sys.argv[3], 'r')
+    @staticmethod
+    def encode(self, password, iv, content):
+        password = extend_string_to_base(password, 16)
+        iv = extend_string_to_base(iv, 16)
 
-input = extend_string_to_base(source_file.read(), 16)
+        content = extend_string_to_base(content, 16)
 
-print( input )
+        obj = AES.new(password, AES.MODE_CBC, iv)
+        return obj.encrypt(input)
 
-obj = AES.new(password, AES.MODE_CBC, iv)
-cipher = obj.encrypt(input)
-print(cipher)
+    @staticmethod
+    def decode(self, password, iv, content):
+        password = extend_string_to_base(password, 16)
+        iv = extend_string_to_base(iv, 16)
 
-file = open(sys.argv[3].split('.')[0], 'wb')
-file.write(cipher)
+        obj = AES.new(password, AES.MODE_CBC, iv)
+        return obj.decrypt(content)
