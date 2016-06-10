3a29043ea804c936e6ee04609c417e06328ff145
Martin Fischer <martin.fis@gmx.net>
2016-06-10 12:54:10 +0200

    adds guard class

--- guard.py
+++ guard.py
@@ -1,4 +1,30 @@
+import sys
 from gui import MainWidget
+from encoder import Encoder
+from utils import write, read
 
-widget = MainWidget()
-widget.open()
+class Guard:
+    def __init__(self):
+        if len(sys.argv) == 1:
+            self.open_gui();
+
+    def format(self, entries):
+        string = ""
+        for entry in entries:
+            string += (entry + ':' + entries[entry] + '\n\r')
+        return string
+
+    def save(self, entries):
+        string = self.format(entries)
+        filename = entries["Entry-Name"]
+        if filename == "":
+            print("Entry-Name need to be set")
+            return
+        cipher = Encoder.encode("masterpassword", "Salt", string)
+        write(cipher, filename)
+
+    def open_gui(self):
+        widget = MainWidget(self.save)
+        widget.open()
+
+Guard()
