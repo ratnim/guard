17f7c07ffde54ee12a72945a0138ee3c9bdc31ce
Martin Fischer <martin.fis@gmx.net>
2016-06-10 12:55:35 +0200

    renames extent_string_to_base

--- Utils.py
+++ Utils.py
@@ -2,7 +2,7 @@
 
 
 # Extends string until it is a multiple of base
-def extend_string_to_base(string, base):
+def extent_string_to_base(string, base):
     while len(string) % base:
         string += ' '
     return string
